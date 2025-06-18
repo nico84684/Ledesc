5ca6aeaeacba37c4e16dccdfdef3cf1c84397b59
Nicolás Fernández <nicolas.s.fernandez@gmail.com>
2025-06-18 19:02:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,19 +31,16 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content={APP_NAME} />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" /> {/* Asumiendo que tendrás este archivo en public/icons/ */}
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#73A8B8" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#73A8B8" />
 
-        {/* Favicon general para navegadores, apuntando a public/images/ */}
         <link rel="icon" href="/images/icono-alta512.png" type="image/png" />
-
-        {/* Iconos para Apple, apuntando a public/images/ */}
+        
         <link rel="apple-touch-icon" href="/images/icono-alta512.png" />
         <link rel="apple-touch-icon" sizes="192x192" href="/images/icono-alta512.png" />
         <link rel="apple-touch-icon" sizes="512x512" href="/images/icono-alta512.png" />
-        
       </head>
       <body
         className={cn(
