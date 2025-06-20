da80d2a87d9bd9b91738dc0302405ff60c3a3d49
Nicolás Fernández <nicolas.s.fernandez@gmail.com>
2025-06-20 23:04:05 +0000

    [19:46:33.377] Running build in Washington, D.C., USA (East) – iad1 [19:

--- next.config.ts
+++ next.config.ts
@@ -7,13 +7,13 @@ const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
   workboxOptions: {
     disableDevLogs: true,
+    skipWaiting: true,
     runtimeCaching: [
         {
           urlPattern: /^https:\/\/placehold\.co\/.*/i,
