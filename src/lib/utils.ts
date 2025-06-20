ad5263211db984eb90832891ecbe6702e6416cc0
Nicolás Fernández <nicolas.s.fernandez@gmail.com>
2025-06-20 19:54:30 +0000

    [16:51:00.863] Running build in Washington, D.C., USA (East) – iad1 [16:

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { format, parseISO } from 'date-fns';
+import { format, parseISO, type Locale } from 'date-fns';
 import { es } from 'date-fns/locale';
 
 export function cn(...inputs: ClassValue[]) {
