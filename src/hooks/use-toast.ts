75d48551a28e8093f2d9ef08539631621a5951af
Nicolás Fernández <nicolas.s.fernandez@gmail.com>
2025-06-21 17:43:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,4 @@
+
 "use client"
 
 // Inspired by react-hot-toast library
@@ -184,10 +185,15 @@ function useToast() {
     }
   }, [state])
 
+  const dismiss = React.useCallback(
+    (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+    []
+  );
+
   return {
     ...state,
     toast,
-    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+    dismiss,
   }
 }
 
